4e126ef6508a712b25d0a123fd6e1148074300f7
unknown <pjconnolly12@gmail.com>
2019-01-16 21:56:07 -0500

    adding bookmarklet button

---
+++ social/images/static/js/bookmarklet.js
@@ -0,0 +1,55 @@
+(function(){
+	var jquery_version = '3.3.1';
+	var site_url = 'https://127.0.0.1:8000/';
+	var static_url = site_url + 'static/';
+	var min_width = 100;
+	var min_height = 100;
+
+	function bookmarklet(msg) {
+		// load CSS
+		var css = jQuery('<link>');
+		css.attr({
+			rel: 'stylesheet',
+			type: 'text/css',
+			href: static_url + 'css/bookmarklet/css?r=' + Math.floor(Math.random()*99999999999999999999)
+		});
+		jQuery('head').append(css);
+
+		// load HTML
+		box_html = '<div id="bookmarklet"><a href="#" id="close">&times;</a><h1>Select an image to bookmark:</h1><div class="images"></div></div>';
+		jQuery('body').append(box_html);
+
+		// close event
+		jQuery('#bookmarklet#close').click(function(){
+			jQuery('#bookmarklet').remove();
+		});
+	};
+
+	// Check if jquery is loaded
+	if(typeof window.jQuery != 'undefined') {
+		bookmarklet();
+	} else {
+		// Check for conflicts
+		var conflict = typeof window.$ != 'undefined';
+		// Create the script and point to Google API
+		var script = document.createElement('script');
+		script.src = '//ajax.googleapis.com/ajax/libs/jquery/' + jquery_version + '/jquery.min.js';
+		// Add the script to the 'head' for processing
+		document.head.appendChild(script);
+		// Create a way to wait until script loading
+		var attempts = 15;
+		(function(){
+			// Check again if jQuery is undefined
+			if(typeof window.jQuery == 'undefined') {
+				if(--atempts > 0) {
+					//Calls himself in a few milliseconds
+					window.setTimeout(arguments.callee,250)
+				} else {
+					// Too much attempts to load, send error
+					alert('An error ocurred while loading jQuery')}
+			} else {
+				bookmarklet();
+			}
+		})();
+	}
+})()
--- social/social/settings.py
+++ social/social/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'social_django',
+    'images.apps.ImagesConfig',
 ]
 
 MIDDLEWARE = [
@@ -107,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'US/Eastern'
 
 USE_I18N = True
 
